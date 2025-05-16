048e841b8668349980dd372326e834b54e411488
Kanmi Obasa <kanmi@konfirmed.com>
2025-05-16 13:55:50 -0700

    fix: update LICENSE_ENDPOINT URL to new domain

--- packages/kanmi-perf-pro/src/kanmi-perf-pro.js
+++ packages/kanmi-perf-pro/src/kanmi-perf-pro.js
@@ -2,7 +2,7 @@ import { KanmiPerfConfig } from './config.js';
 // ────────────────────────────────────────────────────────────────────────────
 //  LICENSING
 // ────────────────────────────────────────────────────────────────────────────
-const LICENSE_ENDPOINT = 'https://knfrmd.com/api/license/validate';
+const LICENSE_ENDPOINT = 'https://www.knfrmd.com/api/license/validate';
 let __kanmiPerfLicenseValid = false;
 
 /**
